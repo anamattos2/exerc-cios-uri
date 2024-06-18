08ac6e1a718faa672cbbd9349221d87ecece713c
Anà Mattos <anapaula_gdm@hotmail.com>
2024-06-18 12:04:24 -0300

    exercicio38

---
+++ Exercicio38.py
@@ -0,0 +1,12 @@
+nota = int(input())
+
+if 86 <= nota <= 100:
+    print("A")
+elif 61 <= nota <= 85:
+    print("B")
+elif 36 <= nota <= 60:
+    print("C")
+elif 1 <= nota <= 35:
+    print("D")
+else:
+    print("E")
