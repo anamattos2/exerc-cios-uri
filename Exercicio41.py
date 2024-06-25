1cb02ce49ac8b1cb3c748ae6d3f4b08a719eb432
Anà Mattos <anapaula_gdm@hotmail.com>
2024-06-25 10:58:23 -0300

    exercicio41

---
+++ Exercicio41.py
@@ -0,0 +1,10 @@
+quantidade = int(input())
+lista = []
+for i in range (0,quantidade):
+    nome_pokemon = input()
+    lista.append(nome_pokemon)
+
+lista_nova = list(set(lista))
+quantidade = len(lista_nova)
+print(lista)
+print(f"Falta(m) {151-quantidade} pomekon(s).")
